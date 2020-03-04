7f247e34945e3a27c752d47f70635c2d2c1868c5
SashaMois <56739595+SashaMois@users.noreply.github.com>
2020-03-04 22:02:14 +0300

    Create project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Star_tree
+{
+    class Program
+    {
+
+        static void DrawPyramid(int lines)
+        {
+            lines = (lines > 115) ? 115 : lines;
+
+            int i = 0; // counter
+            int middle = 0; // middle position for first star
+
+            int s; // variable for find middle pozition for first star
+            for (s = 1; i < lines; ++i, s += 2)
+                ;
+            middle = (s - 2) / 2;
+
+            for (s = 1, i = 0; i < lines; ++i, Console.WriteLine())
+            {
+                for (int j = 0; j < middle; ++j)
+                    Console.Write(' ');
+                --middle;
+
+                for (int j = 0; j < s; ++j)
+                    Console.Write('*');
+                s += 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            /* Input a tree */
+            Console.Write("Input count of lines: ");
+            Console.WriteLine();
+            DrawPyramid(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine("Cheers holiday!");
+            /****************/
+
+            Console.ReadKey(); // waiting finish input
+        }
+    }
+}
